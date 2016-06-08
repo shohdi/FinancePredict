e10d03caf8002074244c347e9efc5fefc5731c41
shohd <shohdi@gmail.com>
2016-06-08 19:52:58 +0300

    First version 0.0.6

---
+++ FinanceTest/FinanceTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinanceTest.MvcApplication" Language="C#" %>
---
+++ FinanceTest/FinanceTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanceTest.Startup))]
+namespace FinanceTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
